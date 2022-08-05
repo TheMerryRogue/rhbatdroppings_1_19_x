c1268cc8dfb6c99c4415694a9c6508db9f651440
The Merry Rogue <108585847+TheMerryRogue@users.noreply.github.com>
2022-08-05 15:57:25 -0700

    Initial commit for 1.19.2 version
    Original uploaded version was 1.19.0 built on an old 1.18 KaupenJoe tutorial mod. This is the updated version built from the 1.19 example mod.

---
+++ main/java/net/merryrogue/rhbatdroppings/BatDroppingsClient.java
@@ -0,0 +1,11 @@
+package net.merryrogue.rhbatdroppings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BatDroppingsClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
